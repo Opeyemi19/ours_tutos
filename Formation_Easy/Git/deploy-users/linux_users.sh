48a3100a282bb0a6d6266fa89c334daec8bc48ba
Opeyemi19 <47104102+Opeyemi19@users.noreply.github.com>
2021-05-18 07:27:37 +0000

    Usage du git squash pour rendre en Un les commits avec commande git rebase -i ID_Commit
    Exply the details
    
    add please in password
    
    Update index after use git stash in this modification

--- Formation_Easy/Git/deploy-users/linux_users.sh
+++ Formation_Easy/Git/deploy-users/linux_users.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
+	read -p "Enter your username in this : " username
+	read -s -p "Enter password please please !!! : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
 		echo "$username exists!"
